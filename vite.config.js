9ba85f4f7c6f95006b3ae90c9bd6f77dca93d5c9
VivanRajath <vivanrajath999@gmail.com>
2025-11-29 13:55:55 +0530

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/VS-Tobacco/',   // <-- add this line
+  base: '/VS-Tobacco/',
   server: {
     host: '0.0.0.0',
-    port: 5173,
-    hmr: {
-      clientPort: undefined
-    }
+    port: 5173
   }
 })
